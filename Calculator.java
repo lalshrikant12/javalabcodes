059bea977cce7f712e24dfce2e1d01dff4cd98c7
Shri Kant Lal <46701610+lalshrikant12@users.noreply.github.com>
2020-01-16 22:13:18 +0530

    Add files via upload
    four function calculator using command line arguments

---
+++ Calculator.java
@@ -0,0 +1,35 @@
+public class Calculator
+{
+public static void main(String[] arg)
+{
+int a=Integer.parseInt(arg[0]);
+String op = arg[1];
+
+int b=Integer.parseInt(arg[2]);
+int z;
+if(op.equals("+"))
+{z=(a+b);
+
+}
+else if(op.equals("-"))
+{z=(a-b);}
+
+else if(op.equals("*"))
+{z=(a*b);
+
+}
+else if(op.equals("/"))
+{z=(a/b);
+
+}
+else {
+throw new java.lang.Error("operator not found");
+
+}
+System.out.println(z);
+
+
+
+}
+
+}
