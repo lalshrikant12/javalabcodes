9328857140c2324a8572975482e2dbb5994a0420
Shri Kant Lal <46701610+lalshrikant12@users.noreply.github.com>
2020-01-16 22:16:38 +0530

    Add files via upload
    Greatest of three numbers using commandline

---
+++ Three.java
@@ -0,0 +1,24 @@
+public class Three
+{
+public static void main(String[] arg)
+{
+int a=Integer.parseInt(arg[0]);
+int b=Integer.parseInt(arg[1]);
+int c=Integer.parseInt(arg[2]);
+if(a>=b && a>=c)
+{
+System.out.println(a+ "a is greatest");
+
+}
+else if(b>=a && b>=c)
+{
+System.out.println(b+ "b is greatest ");
+
+}
+else
+{
+System.out.println(c +"c is greatest ");
+}
+
+}
+}
