fbdd5d997811d67b12fc4b0f101176908a1d9942
Shri Kant Lal <46701610+lalshrikant12@users.noreply.github.com>
2020-01-16 22:15:36 +0530

    Add files via upload
    fibonacci series using commandline

---
+++ Fibonacci.java
@@ -0,0 +1,16 @@
+public class Fibonacci
+{
+public static void main(String arg[])
+{
+int n=Integer.parseInt(arg[0]);
+int a=0,b=1,c;
+for(int i=2;i<n;i++)
+{
+c=a+b;
+System.out.println("Fibinacci series "+c);
+a=b;
+b=c;
+}
+
+}
+}
