b8effef10f144abc5ca5efbd8acfaa68a71f7779
Shri Kant Lal <46701610+lalshrikant12@users.noreply.github.com>
2020-01-16 22:11:26 +0530

    Add files via upload
    Addidition of two numbers using command line arguments

---
+++ Addition.java
@@ -0,0 +1,16 @@
+public class Addition
+{
+public static void main(String[] arg)
+{
+int a=Integer.parseInt(arg[0]);
+int b=Integer.parseInt(arg[1]);
+int c;
+c=a+b;
+System.out.println("sum is  "+c);
+
+
+
+
+
+}
+}
