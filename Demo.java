23ed0b036ed3cd6f55c09d9b9fccaca403dd4239
Shri Kant Lal <46701610+lalshrikant12@users.noreply.github.com>
2020-01-21 20:02:50 +0530

    Create Demo.java

---
+++ Demo.java
@@ -0,0 +1,18 @@
+/** this is code to count number of objects created */
+public class Demo
+{
+int count;
+Demo()
+{
+count ++;
+System.out.println(+count);
+}
+
+
+public static void main(String arg[])
+{
+Demo obj1=new Demo();
+Demo obj2=new Demo();
+
+}
+}
