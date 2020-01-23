67475103f6da3a520c30b10d0e1ecfbf55435cb7
Shri Kant Lal <46701610+lalshrikant12@users.noreply.github.com>
2020-01-23 22:56:20 +0530

    Create Grade.java

---
+++ Grade.java
@@ -0,0 +1,61 @@
+import java.util.Scanner
+class Marks
+{
+int mark;
+string grade
+calculate_grade(obj[i].a)
+{
+
+if(marks>=40 && a<=50)
+{
+grade =p;
+}
+else if(marks>50)
+{
+grade="merit";
+}
+else  if(marks>40)
+{
+grade=="pass";
+}
+else
+grade="fail"
+
+}
+int temp=0;
+public class ChangeGrade
+{
+public static void main(String arg[])
+{
+Scanner sc = new Scanner(System.in)
+
+Marks obj[]=new Marks[10];
+for(int i=0;i<10;i++)
+{
+obj[i].mark=Sc.nextInt();
+obj[i].CalculateGrade();
+
+}
+for(int i=0;i<n-2;i++)
+for(int j=0;j<n-i-1;j++)
+{
+if(obj[j].mark>obj[j+1].mark)
+{
+temp=obj[j].mark;
+obj[j].mark=obj[j+1].mark;
+obj[j+1].mark=temp;
+}
+}
+for(int i=0;i<10;i++)
+{
+System.out.println(obj[i].mark)
+
+}
+
+
+
+
+}
+
+
+}
